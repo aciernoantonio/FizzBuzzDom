a2ca04ced08a7b0d7b9f8b5cbcc91a6ff3007b3e
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-28 14:14:15 +0200

    add number print js section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-const fizzElement = document.querySelector(".fizz");
 const numbersElement = document.querySelector(".numbers")
 
 
@@ -8,11 +7,20 @@ for (let i = 1; i <= 100; i++) {
     
 
     if (i % 3 == 0) {
-       /* console.log(fizzElement); */
-       fizzElement.innerHTML += `Fizz`
+       //se i è multiplo di 3 stampa Fizz
+       numbersElement.innerHTML += `Fizz`
+
+    } else if (i % 5 == 0) {
+        //se i è multiplo di 5 stampa buzz
+        numbersElement.innerHTML += `Buzz`
+
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        //se i è multiplo sia di 3 che di 5 stampa fizzbuzz
+        numbersElement.innerHTML+= `FizzBuzz`
 
     } else {
-        numbersElement.innerHTML += `${i} `
+        //altrimenti stampa i
+        numbersElement.innerHTML += `${i}`
     }
 }
 
