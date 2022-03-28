aed1ec3bb73699447bac182c5225cbe3a078dae1
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-28 15:09:05 +0200

    add script and layout section

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,27 @@ for (let i = 1; i <= 100; i++) {
     /* console.log(i); */
     
 
-    if (i % 3 == 0) {
+    if(i % 3 == 0 && i % 5 == 0) {
+    //se i è multiplo sia di 3 che di 5 stampa fizzbuzz
+    console.log("FizzBuzz");
+    numbersElement.innerHTML+= `<div class="fizzbuzz square">FizzBuzz</div>`
+
+    } else if (i % 3 == 0) {
+        console.log("Fizz")
        //se i è multiplo di 3 stampa Fizz
-       numbersElement.innerHTML += `Fizz`
+       numbersElement.innerHTML += `<div class="fizz square">Fizz</div>`
+
 
     } else if (i % 5 == 0) {
+        console.log("Buzz")
         //se i è multiplo di 5 stampa buzz
-        numbersElement.innerHTML += `Buzz`
+        numbersElement.innerHTML += `<div class="buzz square">Buzz</div>`
 
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        //se i è multiplo sia di 3 che di 5 stampa fizzbuzz
-        numbersElement.innerHTML+= `FizzBuzz`
 
     } else {
+        console.log(`${i}`)
         //altrimenti stampa i
-        numbersElement.innerHTML += `${i}`
+        numbersElement.innerHTML += `<div class="ordinary square">${i}</div>`
     }
 }
 
